26daaa40af8c357c4086255596c1c8c7fb08d542
Ahrorbek Davronov <axrorbektatu@gmail.com>
2024-03-20 16:34:24 +0500

    jira-clone

--- components/layouts/main-navbar.vue
+++ components/layouts/main-navbar.vue
@@ -2,8 +2,9 @@
 <template>
     <div class="h-[10vh] fixed top-0 left-0 right-0 bg-gray-100 dark:bg-gray-900">
         <div class="container mx-auto flex items-center h-full justify-between">
-            <nuxt-link to="/">
-                <NuxtImg />
+            <nuxt-link to="/" class="flex items-center space-x-1">
+                <NuxtImg src="/logo.svg" width="50" height="50" />
+                <span class="text-2xl font-medium">Jira software</span>
             </nuxt-link>
         </div>
     </div>
